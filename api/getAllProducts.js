18275c09aa759a9943ac872918b0ecc085860a58
CoderShahzaib <your-github-email@example.com>
2025-09-26 20:54:32 +0500

    product api fiexed

--- api/getAllProducts.js
+++ api/getAllProducts.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://freeapi.miniprojectideas.com/api/EcommerceApp/GetAllProducts"); 
+    const response = await fetch("https://freeapi.miniprojectideas.com/api/amazon/GetAllProducts"); 
     const data = await response.json();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
