00673cf1f27522437e290ecb7517574906b2383e
CoderShahzaib <your-github-email@example.com>
2026-01-07 10:51:07 +0500

    Add environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://shopsy.runasp.net/api/v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://shopsy.runasp.net/api/v1',
+  stripePublicKey: 'pk_test_51Se9SL4AlCeaxIwY7H4BN840pMHnCbkyDVpDR4eMSj1LsZTcFTWbbWVOohqXta8oDgYNWdx4V90TbazW893Yew3Q00NMRef0FN'
+};
+
